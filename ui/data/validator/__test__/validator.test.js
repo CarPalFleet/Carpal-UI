9c90649e10fd50706ef0f5ac48d15e7248666d56
Dede <winsandymyint@gmail.com>
2018-03-06 10:46:51 +0800

    add sample test case in validator.test.js

---
+++ ui/data/validator/__test__/validator.test.js
@@ -0,0 +1,8 @@
+import { validator } from '../Validator';
+
+describe('Retrieve whitelabel', () => {
+  it('Should get driver details', async () => {
+    let result = validator.changeDriverSlot();
+    expect(true).toBeTruthy();
+  });
+});
