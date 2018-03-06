80eb913e35ac852ceac13ce2384083b176eac473
JingeXC <cbb467cp01@163.com>
2018-03-06 14:51:53 +0800

    modify

--- src/data/routing/Routing.js
+++ src/data/routing/Routing.js
@@ -1,33 +1,31 @@
-export var calculate = function(data) {
+export var calculate_driver_usage = function(data) {
   if (!data) {
-    throw 'data is enpty';
+    throw 'data is empty';
   }
   if (!data.driver_schedules || !data.driver_schedules.length) {
-    throw 'driver_schedules is enpty';
+    throw 'driver_schedules is empty';
   }
   if (!data.driver_assignments || !data.driver_assignments.length) {
-    throw 'driver_assignments is enpty';
+    throw 'driver_assignments is empty';
   }
 
   let result = [];
 
-  let driver_schedules_time_array = [];
+  let driver_schedules_time = null;
   for (let schedules of data.driver_schedules) {
     let start_time = new Date(schedules.start_at + " " + schedules.start_time).getTime();
     let end_time = new Date(schedules.start_at + " " + schedules.end_time).getTime();
-    driver_schedules_time_array.push(end_time - start_time);
+    driver_schedules_time +=(end_time - start_time);
   }
 
-  let driver_assignments_array = [];
+  let driver_assignments = null;
   for (let assignments of data.driver_assignments) {
     let assignment_start = new Date(assignments.assignment_start).getTime();
     let assignment_end = new Date(assignments.assignment_end).getTime();
-    driver_assignments_array.push(assignment_end - assignment_start);
+    driver_assignments +=(assignment_end - assignment_start);
   }
 
-  for (let i = 0,len = driver_schedules_time_array.length; i < len ; i++ ){
-    result.push((driver_assignments_array[i]/driver_schedules_time_array[i]).toFixed(4));
-  }
+  result = (driver_assignments/driver_schedules_time).toFixed(4);
 
   return result;
 }
