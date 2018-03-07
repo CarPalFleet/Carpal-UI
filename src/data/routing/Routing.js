34fed58eade277da95df8cf3984b467915d8b1a2
JingeXC <cbb467cp01@163.com>
2018-03-07 10:24:22 +0800

    Improve code base on comments

--- src/data/routing/Routing.js
+++ src/data/routing/Routing.js
@@ -12,35 +12,44 @@ Calculate the usage % of driver.
 
 */
 
-export var calculateDriverUsage = function(data) {
+export const calculateDriverUsage = (data) => {
+    class exception {
+        constructor(message) {
+            this.message = message;
+        }
+    }
+    try {
+        if (!data) {
+            throw new exception('data is empty');
+        }
+        if (!data.driver_schedules || !data.driver_schedules.length) {
+            throw new exception('driver_schedules is empty');
+        }
+        if (!data.driver_assignments || !data.driver_assignments.length) {
+            throw new exception('driver_assignments is empty');
+        }
 
-    if (!data) {
-        throw 'data is empty';
-    }
-    if (!data.driver_schedules || !data.driver_schedules.length) {
-        throw 'driver_schedules is empty';
-    }
-    if (!data.driver_assignments || !data.driver_assignments.length) {
-        throw 'driver_assignments is empty';
-    }
+        let driverSchedulesTime = null;
+        for (let schedules of data.driver_schedules) {
+            let startTime = new Date(`${schedules.start_at} ${schedules.start_time}`).getTime();
+            let endTime = new Date(`${schedules.start_at} ${schedules.end_time}`).getTime();
+            driverSchedulesTime += (endTime - startTime);
+        }
 
-    let driverSchedulesTime = null;
-    for (let schedules of data.driver_schedules) {
-        let startTime = new Date(schedules.start_at + " " + schedules.start_time).getTime();
-        let endTime = new Date(schedules.start_at + " " + schedules.end_time).getTime();
-        driverSchedulesTime += (endTime - startTime);
-    }
+        let driverAssignmentsTime = null;
+        for (let assignments of data.driver_assignments) {
+            let assignmentStart = new Date(assignments.assignment_start).getTime();
+            let assignmentEnd = new Date(assignments.assignment_end).getTime();
+            driverAssignmentsTime += (assignmentEnd - assignmentStart);
+        }
 
-    let driverAssignmentsTime = null;
-    for (let assignments of data.driver_assignments) {
-        let assignmentStart = new Date(assignments.assignment_start).getTime();
-        let assignmentEnd = new Date(assignments.assignment_end).getTime();
-        driverAssignmentsTime += (assignmentEnd - assignmentStart);
-    }
+        if (!driverSchedulesTime) {
+            throw new exception('The denominator cannot be zero.');
+        }
 
-    if (!driverSchedulesTime) {
-        throw 'The denominator cannot be zero.';
-    }
+        return (driverAssignmentsTime / driverSchedulesTime).toFixed(4);
 
-    return (driverAssignmentsTime / driverSchedulesTime).toFixed(4);
+    } catch (e) {
+        console.log(e.message);
+    }
 }
